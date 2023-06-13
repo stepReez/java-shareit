a4164ccc095c74c24ba23d07c6fe75e92224f026
sReez <step.reznikoff@yandex.ru>
2023-06-13 19:41:57 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -4,7 +4,7 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 public class UserMapper {
-    public static UserDto toUserDto (User user) {
+    public static UserDto toUserDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
                 .name(user.getName())
